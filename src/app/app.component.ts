949dc24c22e554210784761078e3003a47a3e873
SolidJulien <31781936+judev34@users.noreply.github.com>
2022-10-14 12:05:12 +0200

    create list of pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ng-pokemon-app';
+
+  pokemonList: Pokemon[] = POKEMONS;
+
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit(): void {
+    console.log(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string ) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id === +pokemonId);
+    if (pokemon) {
+      console.log(`Vous avez selectionné le pokémon ${pokemon.name} !`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez selectionné un fake pokémon !`);
+      this.pokemonSelected = pokemon;
+    } 
+  }
+
 }
