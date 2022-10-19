7ede07c59d80c3aee96aa153bf3f926be5fec13e
judev34 <julien.coduert34@gmail.com>
2022-10-19 15:34:36 +0200

    create detail pokemon detail view

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
+  styleUrls: ['./detail-pokemon.component.css']
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemonList!: Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    this.pokemonList = POKEMONS;
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
 
+    if (pokemonId) {
+      this.pokemon =  this.pokemonList.find(pokemon => pokemon.id === +pokemonId);
+    } 
+  }
 }
