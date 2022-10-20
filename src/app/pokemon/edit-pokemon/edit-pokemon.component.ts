65acdafbe15985ce81023e694940754327fefd0f
judev34 <julien.coduert34@gmail.com>
2022-10-20 16:13:18 +0200

    add form and edit parent component

---
+++ src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-edit-pokemon',
+  template: `
+    <h2>Editer {{ pokemon?.name }}</h2>
+    <div *ngIf="pokemon" class="picture-container">
+      <img [src]="pokemon.picture">
+    </div>
+    <app-pokemon-form *ngIf="pokemon" [pokemon]="pokemon"></app-pokemon-form>
+  `,
+  styles: [
+  ]
+})
+export class EditPokemonComponent implements OnInit {
+
+  pokemon: Pokemon|undefined;
+
+  constructor(
+    private pokemonService: PokemonService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    console.log(this.pokemon);
+    
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    if (pokemonId) {
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+    } else {
+      this.pokemon = undefined;
+    }
+
+  }
+
+}
--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
 const routes: Routes = [
+    { path: 'edit/pokemon/:id', component: EditPokemonComponent },
     { path: '', component: ListPokemonComponent },
     { path: ':id', component: DetailPokemonComponent},
 ];
