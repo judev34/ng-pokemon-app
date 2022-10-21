8b852ae68428fac68f483f4459a113720e7d0a2c
judev34 <julien.coduert34@gmail.com>
2022-10-21 12:40:07 +0200

    add landing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  
   { path: 'pokemons', loadChildren: () => import('./pokemon/pokemon.module').then(m => m.PokemonModule) },
-  { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
+  { path: '', component: LandingPageComponent },
   { path: '**', component: PageNotFoundComponent } // à déclrarer en dernier
 
 ];
