49e55f8ec519a5ef868d4d99f3de0aec94ab93ca
SolidShnek <julien.coudert34@gmail.com>
2023-01-07 10:43:31 +0100

    add fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    PokemonModule
+    PokemonModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
